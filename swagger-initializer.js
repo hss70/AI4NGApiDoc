80abe427c0e0e67d7333f2495a4034391770739e
Hardeep Sidhu <hss70@bath.ac.uk>
2025-05-22 19:18:43 +0100

    feat: add Classifier API documentation and update Jekyll configuration

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./assets/Apis/classifierSwagger.yaml",
+    url: "./Apis/classifierSwagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
